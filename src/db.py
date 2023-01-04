bdd0022f49df5b9938bfa1b199006442281e22f5
alessiogandelli <alessiogandelli99@gmail.com>
2023-01-04 04:04:24 +0100

    get bus stops from db

--- src/db.py
+++ src/db.py
@@ -2,6 +2,7 @@
 import psycopg2
 import dotenv 
 import os
+from shapely import wkb
 dotenv.load_dotenv()
 
 # Connect to the database
@@ -11,7 +12,7 @@ conn = psycopg2.connect(database='geospatial', user='osmone' , password=os.envir
 cur = conn.cursor()
 
 # Execute a SQL query to retrieve the OSM data
-query = "SELECT * FROM planet_osm_point"
+query = "SELECT name, ST_AsBinary(way) FROM planet_osm_point"
 cur.execute(query)
 
 # Fetch the results of the query
@@ -19,9 +20,45 @@ results = cur.fetchall()
 
 # Print the results
 for result in results:
-    print(result)
+    point = wkb.loads(result[1].hex(), hex=True)
+    print(result[0], point)
 
 # Close the cursor and connection
 cur.close()
 conn.close()
+# %%
+
+class Database:
+    def __init__(self):
+        print('connecting to default database ...')
+        self._conn = psycopg2.connect(database='geospatial', user='osmone' , password=os.environ['db_pass'], host='localhost')
+        self._cursor = self._conn.cursor()
+
+        print('connected to db ' )
+
+
+    def get_bus_stops(self):
+        query = "SELECT name, ST_AsBinary(way) FROM planet_osm_point"
+        self._cursor.execute(query)
+        results = self._cursor.fetchall()
+
+        stops = {}
+
+        for result in results:
+            stops[result[0]] = wkb.loads(result[1].hex(), hex=True).wkt # translate binary to shapely geometry
+
+        return stops
+
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        self._conn.close()
+    
+
+
 # %%
